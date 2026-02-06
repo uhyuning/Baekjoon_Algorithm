05bcce45e3e376116f457b11c4daf04eb2f09ea0
uhyuning <86033853+uhyuning@users.noreply.github.com>
2026-02-06 15:03:06 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#pragma warning(disable: 4996)
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		for (int k = 0; k < n - i; k++)
+			printf(" ");
+		for (int k = 0; k < i + (i - 1); k++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	for (int i = n - 1; i > 0; i--) {
+		for (int k = 0; k < n - i; k++)
+			printf(" ");
+		for (int k = 0; k < i + (i - 1); k++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	return 0;
+}
