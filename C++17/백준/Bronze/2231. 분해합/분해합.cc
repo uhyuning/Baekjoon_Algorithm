397537433b0f9633b1025de335cbc5e09aa3d6c5
uhyuning <86033853+uhyuning@users.noreply.github.com>
2026-04-17 13:01:11 +0900

    [Bronze II] Title: 분해합, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    
+    scanf("%d", &N);
+    
+    for (int i = 1; i < N; i++)
+    {
+        int temp = i;
+        int num = i;
+        
+        while (temp > 0) {
+            num += temp % 10; // 마지막 자릿수를 더함
+            temp /= 10;       // 마지막 자릿수를 제거
+        }
+        
+        if (num == N)
+        {
+            printf("%d", i);
+            N = 0;
+            break;
+        }
+    }
+    
+    if (N != 0)
+        printf("0");
+}
